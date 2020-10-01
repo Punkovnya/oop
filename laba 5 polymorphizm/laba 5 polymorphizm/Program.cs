bb720a990bed708b3e6c20962f58a0ee57f7e1de
CoolDeath <dobre4ko@gmail.com>
2020-10-02 01:50:10 +0300

    5 лаба и изменения 4

---
+++ laba 5 polymorphizm/laba 5 polymorphizm/Program.cs
@@ -0,0 +1,60 @@
+﻿
+using System;
+
+namespace laba_5_polymorphizm
+{
+    class Program
+    { 
+        public abstract class Transport
+        {
+            private int weight;
+            public abstract int Weight
+            {
+                get;set;
+            }
+           
+
+        }
+        public class Aviation:Transport
+        {
+            private int weight;
+            public override int Weight
+            {
+                get
+                {
+                    return weight;
+                }
+                set
+                {
+                    weight = value;
+                }
+            }
+            public class Cargo
+            {
+               
+            }
+            public class Military
+            {
+                public class Tu134
+                {
+
+                }
+            }
+            public class Passenger
+            {
+                public class Boeing
+                {
+
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Aviation.Cargo cargo = new Aviation.Cargo();
+            Aviation aviation = new Aviation();
+            aviation.Weight = 10;
+                
+        }
+    }
+}
